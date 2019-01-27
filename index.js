a734880f08a5d5a7c14d3006799a4817d0f8aca8
sebasjara <sebas.jara1@hotmail.com>
2019-01-26 19:07:38 -0500

    Dependencias: express body-parser jwt nodemon mongoose

--- index.js
+++ index.js
@@ -1,23 +1,52 @@
 'use strict'
-//Importar dependencias
+//----Importar dependencias----
+// Framework para el servidor
 const express = require('express');
+// Complmento para trabajar con tokens
 const jwt = require('jsonwebtoken');
+// Complem. para convertir peticiones en JSON
 const bodyParser = require('body-parser');
 
 //Inicializar express
 const app = express();
+// Puerto en el que se ejecuta el servidor
 const port = 3000;
 
-app.use(bodyParser.urlencoded({ ex}))
 
-app.get('/', (req, res)=>{
-    res.json({
-        text: 'API funcionando!!'
-    });
+//Configuración básica de body-parser
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
+app.get('/api/products', (req, res) => {
+    res.status(200).send(
+        { products: [] }
+    );
 });
 
+app.get('/api/products/:productId', (req, res) => {
+
+});
+
+app.post('/api/products', (req, res) => {
+    console.log(req.body);
+    res.status(200).send(
+        { message: 'Producto recibido' }
+    );
+});
+
+app.put('/api/products/:productId', (req, res) => {
+
+});
+
+app.delete('/api/products/:productId', (req, res) => {
+
+});
+
+
+//Correr servidor
 app.listen(port,() => {
-    console.log('Servidor en el puerto: ' + port);
+    console.log(`Servidor en el puerto: ${port}`);
 });
 
 
